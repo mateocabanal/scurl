a9daae5f403d70515ea91f55b9ced4a424bcbb52
mateo <mateo@mail.mateocabanal.ca>
2023-12-07 18:46:35 -0800

    pre-alpha version (first commit)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use safenet::{
     frame::{DataFrame, Frame, InitFrame},
     uuid::Uuid,
+    APPSTATE,
 };
 
 #[derive(Parser)]
@@ -14,23 +15,41 @@ struct Args {
 
     #[arg(short, long, requires = "input")]
     data: Option<String>,
+
+    #[arg(short, long)]
+    verbose: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
+    let log_level = if args.verbose {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+    simple_logger::SimpleLogger::new()
+        .with_level(log_level)
+        .env()
+        .init()
+        .unwrap();
+
     if args.method.as_str() == "POST" && args.data.is_none() {
-        panic!("Cannot send POST request without any data! (use --data)");
+        log::error!("Cannot send POST request without any data! (use --data)");
     }
+
+    let uuid = APPSTATE.read().unwrap().uuid.to_string();
 
     let base_url_split = args.url.split('/').collect::<Vec<&str>>();
     let base_url =
         base_url_split.first().unwrap().to_string() + "//" + *base_url_split.get(2).unwrap();
 
     let conn_init_url = format!("{base_url}/conn/init");
+    log::debug!("conn_init url: {conn_init_url}");
 
     let conn_init_frame = InitFrame::default();
     let conn_init_res = minreq::post(conn_init_url)
+        .with_header("s-uuid", &uuid)
         .with_body(conn_init_frame.to_bytes())
         .with_timeout(1000)
         .send();
@@ -44,7 +63,7 @@ fn main() {
             };
 
             let data_res = match method {
-                minreq::Method::Get => minreq::get(args.url).send(),
+                minreq::Method::Get => minreq::get(args.url).with_header("s-uuid", &uuid).send(),
                 minreq::Method::Post => {
                     let mut data_frame = DataFrame::new(args.data.as_ref().unwrap().as_bytes());
                     data_frame
@@ -56,6 +75,8 @@ fn main() {
                 }
                 _ => minreq::get(args.url).send(),
             };
+
+            log::debug!("data res: {data_res:?}");
 
             if let Ok(data_res) = data_res {
                 let mut res_data_frame = DataFrame::from_bytes(data_res.as_bytes()).unwrap();
