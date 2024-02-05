5f8334de6594dc0231c8cd9719e67d6b26847d92
Mateo <mateo@mail.mateocabanal.ca>
2024-02-05 16:21:54 -0600

    Add timers to check speed of operations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use safenet::{
+    app_state::AppState,
     frame::{DataFrame, Frame, InitFrame},
     uuid::Uuid,
     APPSTATE,
@@ -20,11 +21,16 @@ struct Args {
     verbose: bool,
 }
 
+#[cfg(any(debug_assertions, feature = "time"))]
+use std::time::Instant;
+
 fn main() {
+
+    AppState::init().unwrap();
     let args = Args::parse();
 
     let log_level = if args.verbose {
-        log::LevelFilter::Debug
+        log::LevelFilter::Trace
     } else {
         log::LevelFilter::Info
     };
@@ -38,7 +44,7 @@ fn main() {
         log::error!("Cannot send POST request without any data! (use --data)");
     }
 
-    let uuid = APPSTATE.read().unwrap().uuid.to_string();
+    let uuid = APPSTATE.get().unwrap().read().unwrap().uuid.to_string();
 
     let base_url_split = if let Some(url) = args.url.strip_prefix("http://") {
         url
@@ -60,12 +66,18 @@ fn main() {
         .send();
 
     if let Ok(res) = conn_init_res {
+        #[cfg(any(debug_assertions, feature = "time"))]
+        let time = Instant::now();
+
         if conn_init_frame.from_peer(res.as_bytes()).is_ok() {
             let method = match args.method.to_lowercase().as_str() {
                 "get" => minreq::Method::Get,
                 "post" => minreq::Method::Post,
                 _ => minreq::Method::Get,
             };
+
+            #[cfg(any(debug_assertions, feature = "time"))]
+            log::info!("init took {}ms", time.elapsed().as_millis());
 
             let data_res = match method {
                 minreq::Method::Get => minreq::get(args.url).with_header("s-uuid", &uuid).send(),
@@ -74,6 +86,9 @@ fn main() {
                     data_frame
                         .encode_frame(Uuid::from_slice(&res.as_bytes()[3..19]).unwrap())
                         .unwrap();
+
+                    log::trace!("POST data: {:?}", data_frame.body);
+
                     minreq::post(args.url)
                         .with_body(data_frame.to_bytes())
                         .send()
@@ -81,18 +96,34 @@ fn main() {
                 _ => minreq::get(args.url).send(),
             };
 
+            #[cfg(any(debug_assertions, feature = "time"))]
+            let time = Instant::now();
+
             log::debug!("data res: {data_res:?}");
 
             if let Ok(data_res) = data_res {
-                let mut res_data_frame = DataFrame::from_bytes(data_res.as_bytes()).unwrap();
-                res_data_frame.decode_frame().unwrap();
+                let res_data_frame = DataFrame::from_bytes(data_res.as_bytes());
 
-                println!("{}", std::str::from_utf8(&res_data_frame.body).unwrap());
+                if let Ok(mut frame) = res_data_frame {
+                    frame.decode_frame().unwrap();
+
+                    #[cfg(any(debug_assertions, feature = "time"))]
+                    log::info!("data took {}ms", time.elapsed().as_millis());
+
+                    println!("{}", std::str::from_utf8(&frame.body).unwrap());
+                } else {
+                    println!(
+                        "Data received is not a valid Data Frame... printing anyway\n{}",
+                        data_res.as_str().unwrap()
+                    );
+                }
             } else {
                 println!("data request failed...");
             }
+        } else {
+            panic!("Received invalid Initialization Frame!");
         }
     } else {
-        conn_init_res.unwrap();
+        panic!("Failed to connect to http://{base_url}/conn/init");
     }
 }
